e2e5570411fef7fd6b0e1202e4567dc6f52c7035
Patrick Bartels <patrick.bartels@gmail.com>
2013-07-04 09:52:39 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mwrapper::Application.config.secret_token = '4213f49f8df6aac3c1db0aa516a937ae1387fa98990a01f177dcbab8ad17fb408f3a647ef625a56df62859aa3405eecddf85cf891fe4487a1e831609fea9d190'
